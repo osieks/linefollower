07e287535f26e1bf047e3ee20cc5a944a75e5e21
osieks <57353523+osieks@users.noreply.github.com>
2022-03-29 14:23:57 +0200

    Add files via upload

---
+++ QTRARawValues.ino
@@ -0,0 +1,56 @@
+#include <QTRSensors.h>
+
+// This example is designed for use with six analog QTR sensors. These
+// reflectance sensors should be connected to analog pins A0 to A5. The
+// sensors' emitter control pin (CTRL or LEDON) can optionally be connected to
+// digital pin 2, or you can leave it disconnected and remove the call to
+// setEmitterPin().
+//
+// The main loop of the example reads the raw sensor values (uncalibrated). You
+// can test this by taping a piece of 3/4" black electrical tape to a piece of
+// white paper and sliding the sensor across it. It prints the sensor values to
+// the serial monitor as numbers from 0 (maximum reflectance) to 1023 (minimum
+// reflectance).
+
+QTRSensors qtr;
+
+const uint8_t SensorCount = 8;
+uint16_t sensorValues[SensorCount];
+byte p[8];
+bool debug=LOW;
+
+void setup()
+{
+  // configure the sensors
+  qtr.setTypeAnalog();
+  qtr.setSensorPins((const uint8_t[]){A0, A1, A2, A3, A4, A5, A6, A7}, SensorCount);
+  //qtr.setEmitterPin(2);
+
+  Serial.begin(9600);
+}
+
+
+void loop()
+{
+  // read raw sensor values
+  qtr.read(sensorValues);
+  for (uint8_t i = 0; i < SensorCount; i++){
+    p[i]=sensorValues[i]/4;
+  }
+  if(debug==LOW)Serial.write(p,sizeof(p));
+ 
+  if(debug==HIGH){
+    // print the sensor values as numbers from 0 to 1023, where 0 means maximum
+    // reflectance and 1023 means minimum reflectance
+    for (uint8_t i = 0; i < SensorCount; i++)
+    {
+        Serial.print(p[i]);
+        Serial.print('/');
+        Serial.print(sensorValues[i]);
+        Serial.print('\t');
+    }
+    Serial.println();
+  }
+
+  delay(20);
+}
